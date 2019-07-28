1ddf4c3629e95b27019787972c2a97778efe5963
Duong Nguyen <duongntb94@gmail.com>
2019-07-28 20:38:57 +0700

    Updates

---
+++ precache-manifest.fc304022673f229fb59f8d831481ff92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f9fe26d151164baad176a2f8e790e2",
+    "url": "/duongntb94/monster-rolodex/index.html"
+  },
+  {
+    "revision": "32900886af31212fb36a",
+    "url": "/duongntb94/monster-rolodex/static/css/main.d474657e.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/duongntb94/monster-rolodex/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "32900886af31212fb36a",
+    "url": "/duongntb94/monster-rolodex/static/js/main.789a5aa0.chunk.js"
+  },
+  {
+    "revision": "95f634aa9b84639e95b5",
+    "url": "/duongntb94/monster-rolodex/static/js/runtime~main.cbbd9702.js"
+  }
+]);
